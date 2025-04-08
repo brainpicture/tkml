65474893f41cbb630e5173d79ed3ba11ef05eef7
Oleg <oleg@emby.ru>
2025-04-08 20:45:28 +0800

    remove unnecessary file

--- src/components/code.ts
+++
@@ -1,8 +0,0 @@
-// В методе render()
-if (this.runtime) {
-    // Вызов будет выполнен только один раз на странице
-    this.runtime.onInit('loadHighlightJs');
-
-    // Подсвечиваем текущий элемент
-    this.runtime.onInit('highlightElement', elementId);
-} 
